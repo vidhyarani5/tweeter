f9343d520baef19c69fe1baa7dfc5bb34dbcf161
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-28 09:16:29 -0400

    Clean up the code and Common Responsive Design Patterns

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,13 +3,11 @@
 $(document).ready(function() {
   $( "#tweet-text" ).keyup(function(event) {
     let tweetlength = event.target.value.length;
-    console.log(tweetlength);
     $(".counter").text (140 - tweetlength);
     if (tweetlength > 140 ) {
       $(".counter").css("color", "red");
     } else {
       $(".counter").css("color", "grey");
     }
-    console.log(this);
   });
 });
